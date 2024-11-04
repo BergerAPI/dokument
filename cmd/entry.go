c70d603bcf746683dbdefeb84c89af2b019e1c23
BergerAPI <bergerbtw@gmail.com>
2024-11-04 23:04:39 +0100

    feat: basic config

--- cmd/entry.go
+++ cmd/entry.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/BergerAPI/dokument/config"
 	"github.com/BergerAPI/dokument/manifest"
+	"log"
+	"os"
 	"sigs.k8s.io/yaml"
 )
 
 func main() {
-	data, err := yaml.Marshal(manifest.GenerateDeployment(manifest.NewDeploymentConfig("test", "nginx:latest")))
-	if err != nil {
-		println("error marshalling YAML: %w", err)
+	if len(os.Args) != 2 {
+		log.Fatal("Please provide a config file (example: dokument test-dokument.yml)")
 	}
-	fmt.Println(string(data))
+
+	cfg, err := config.LoadConfig(os.Args[1])
+	if err != nil {
+		log.Fatal("Please provide a valid config file (example: dokument test-dokument.yml)")
+	}
+
+	for name, service := range cfg.Services {
+		deployment := manifest.NewDeploymentConfig(name, service.Image)
+		deployment.Replicas = service.Replicas
+
+		data, err := yaml.Marshal(manifest.GenerateDeployment(deployment))
+		if err != nil {
+			println("error marshalling YAML: %w", err)
+		}
+
+		fmt.Println(string(data))
+	}
 }
---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"os"
+	"sigs.k8s.io/yaml"
+)
+
+// Config root node of the config
+type Config struct {
+	Services map[string]Service `yaml:"services"`
+}
+
+// Service defines a single deployment
+type Service struct {
+	Image     string   `yaml:"image"`
+	Replicas  int32    `yaml:"replicas"`
+	Ports     []int32  `yaml:"ports"`
+	Env       []string `yaml:"env"`
+	DependsOn []string `yaml:"depends_on"`
+	Storage   Storage  `yaml:"storage"`
+}
+
+// Storage specifies the volume size of a pvc-claim for a service
+type Storage struct {
+	Size string `yaml:"size"`
+}
+
+// LoadConfig reads the configuration file to generate manifests from
+func LoadConfig(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
